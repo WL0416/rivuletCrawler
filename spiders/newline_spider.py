c11df4bdfe05cf8a74dc690323e93cad7eb2916f
Wei Li <liwsuper7@gmail.com>
2018-08-29 18:42:13 +1000

    category finish

--- spiders/newline_spider.py
+++ spiders/newline_spider.py
@@ -15,15 +15,15 @@ class NewlineSpider(scrapy.Spider):
 
     def parse(self, response):
 
-        page = response.url.split('/')[-2]
+        # page = response.url.split('/')[-2]
         
-        filename = 'pages/%s.html' % page
+        # filename = 'pages/%s.html' % page
         
-        with open(filename, 'wb') as f:
+        # with open(filename, 'wb') as f:
             
-            f.write(response.body)
+        #     f.write(response.body)
         
-        self.log('Saved file %s' % filename)
+        # self.log('Saved file %s' % filename)
         
         crawled_book = Workbook()
         product_sheet = crawled_book.add_sheet('Products', cell_overwrite_ok=True)
@@ -47,10 +47,12 @@ class NewlineSpider(scrapy.Spider):
         category_data = []
         previous_len = None
         parents = []
+        ready_crawl_links = []
         # operate the link string, check the 
         for link_group_index in range(len(links)):
             for link_index in range(len(links[link_group_index])):
-                link_splited = links[link_group_index][link_index].split('/')
+                current_link = links[link_group_index][link_index]
+                link_splited = current_link.split('/')
                 # if link char list's length is 4, it indicates the top category
                 # category is tuple (parent id, category name, category id)
                 current_len = len(link_splited)
@@ -63,6 +65,7 @@ class NewlineSpider(scrapy.Spider):
                         parents.append(str(category_id))
                     elif current_len < previous_len:
                         parents.pop()
+                        ready_crawl_links.pop()
                     category_id += 1
                     parent_str = ''
                     for index in range(len(parents)):
@@ -70,7 +73,11 @@ class NewlineSpider(scrapy.Spider):
                             parent_str += ','
                         parent_str += parents[index]
                     
-                    category = (parent_str, categories[link_group_index][link_index].strip(), str(category_id))
+                    category = (parent_str, 
+                                categories[link_group_index][link_index].strip(), 
+                                str(category_id))
+
+                    ready_crawl_links.append(current_link)
 
                     previous_len = current_len
 
@@ -80,5 +87,12 @@ class NewlineSpider(scrapy.Spider):
             for column_index in range(len(category_data[row_index])):
                 value = category_data[row_index][column_index]
                 category_sheet.write(row_index, column_index, value)
+
+        while len(ready_crawl_links) > 0:
+            
+            next_page = ready_crawl_links.pop()
+            next_page = response.urljoin(next_page)
+            yield scrapy.Request(next_page, callback=self.parse)
+            print(response.css('div.row.products-category').extract())
 
         crawled_book.save('newline_crawled.xls')
